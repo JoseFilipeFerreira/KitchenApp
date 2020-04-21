66c06ed31b60427221c1e3ff8700ff301bbee360
Hitler <a85504@alunos.uminho.pt>
2020-04-21 14:19:07 +0100

    better networking

--- KitchenApp/KitchenLib/JwtBuilder.cs
+++ KitchenApp/KitchenLib/JwtBuilder.cs
@@ -10,7 +10,7 @@ namespace KitchenLib
     public class JwtBuilder
     {
         private static string symSec =
-            "";
+            "J6k2eVCTXDp5b97u6gNH5GaaqHDxCmzz2wv3PRPFRsuW2UavK8LGPRauC4VSeaetKTMtVmVzAC8fh8Psvp8PFybEvpYnULHfRpM8TA2an7GFehrLLvawVJdSRqh2unCnWehhh2SJMMg5bktRRapA8EGSgQUV8TCafqdSEHNWnGXTjjsMEjUpaxcADDNZLSYPMyPSfp6qe5LMcd5S9bXH97KeeMGyZTS2U8gp3LGk2kH4J4F3fsytfpe9H9qKwgjb";
         public static async Task<string> CreateJWTAsync(
             User user,
             string issuer,
