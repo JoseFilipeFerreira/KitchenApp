b962de4750356677de5e7b2ecb64d49581942e17
Manilator <manilator777@gmail.com>
2020-04-27 17:55:13 +0100

    Teste de autenticacao

--- Website/src/App.js
+++ Website/src/App.js
@@ -1,22 +1,60 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
 import Login from './login'
 import Register from './register'
 import Dashboard from './dashboard'
 
+
+
 class App extends Component {
+
+  isAuth = () => {
+    let token = localStorage.getItem('auth');
+    axios
+      .get("http://localhost:1331/inventory/all", {
+        headers: { "auth": token }
+      }, { withCredentials: true })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    console.log('teste');
+
+    if (localStorage.getItem('auth') != null) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   render() {
-    return (
-      <React.Fragment>
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Login}/>
-            <Route path="/register" component={Register}/>
-            <Route path="/dashboard" component={Dashboard}/>
-          </Switch>
-        </Router>
-      </React.Fragment>
-    )
+    if (this.isAuth() === false) {
+      return (
+        <React.Fragment>
+          <Router>
+            <Switch>
+              <Route path="/register" component={Register} />
+              <Route exact path="*" component={Login} />
+            </Switch>
+          </Router>
+        </React.Fragment>
+      )
+    } else {
+      return (
+        <React.Fragment>
+          <Router>
+            <Switch>
+              <Route exact path="/" component={Login} />
+              <Route path="/register" component={Register} />
+              <Route path="/dashboard" component={Dashboard} />
+            </Switch>
+          </Router>
+        </React.Fragment>
+      )
+    }
   }
 }
 
