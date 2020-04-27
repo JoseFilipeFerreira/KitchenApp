bcc66f7d117dc9b0ea60a9dc86a290d89f23f442
Manilator <manilator777@gmail.com>
2020-04-27 16:16:14 +0100

    Routes atualizados

--- Website/src/App.js
+++ Website/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Login from './login'
 import Register from './register'
+import Dashboard from './dashboard'
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={Login}/>
             <Route path="/register" component={Register}/>
+            <Route path="/dashboard" component={Dashboard}/>
           </Switch>
         </Router>
       </React.Fragment>
