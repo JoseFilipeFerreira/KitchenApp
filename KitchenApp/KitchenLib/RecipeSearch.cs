368ca8c44d6ae34b6230933ed24b204f07dac347
JoseFilipeFerreira <36403066+JoseFilipeFerreira@users.noreply.github.com>
2020-04-29 12:36:34 +0100

    improve recipe API wrapper (#70)
    * improve recipe API wrapper
    
    * remove dubug print
    
    * change return type
    
    * move API_KEY to separate var

--- KitchenApp/KitchenLib/RecipeSearch.cs
+++ KitchenApp/KitchenLib/RecipeSearch.cs
@@ -1,40 +1,71 @@
 using System;
 using System.Collections.Generic;
-//Request library
 using System.Net;
 using System.IO;
 using System.Linq;
-using System.Net.Http;
-using System.Threading.Tasks;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace KitchenLib
 {
-    class MinimalRecipe
+    class RootMinimalRecipes
     {
-        public string title { get; set; }
-        public string image { get; set; }
-        public uint id { get; set; }
-        
-        public MinimalRecipe(string recipeTitle, string imageURL, uint recipeId)
-        {
-            title = recipeTitle;
-            image = imageURL;
-            id = recipeId;
-        }
+        public List<MinimalRecipe> results { get; set; }
     }
 
     public class RecipeSearch
     {
-        private List<Product> Ingridients;
-        private string API_KEY = "7a98067ae9ea425ca548d96347913e74";
-
-        private string apiRecepiesIngridients = "https://api.spoonacular.com/recipes/";
-
-        public RecipeSearch(List<Product> ingridients)
+        public static List<Recipe> SearchRecipe(uint number, List<Product> ingridients)
         {
-            Ingridients = ingridients;
+            var options = "number=" + number;
+
+            options += "&includeIngredients=";
+
+            var ingridientsString = ingridients.Select(s => s._name).ToList();
+            options += string.Join(",", ingridientsString);
+            
+            return SearchRecipe(GetMinimalRecipies(options));
+        }
+        
+        public static List<Recipe> SearchRecipe(uint number, string recipeName)
+        {
+            var options = "number=" + number;
+            options += "&query=" + recipeName;
+
+            return SearchRecipe(GetMinimalRecipies(options));
+            
+        }
+        
+        public static List<Recipe> SearchRecipe(uint number, string recipeName, List<Product> ingridients)
+        {
+            var options = "number=" + number;
+            
+            options += "&query=" + recipeName;
+            
+            options += "&includeIngredients=";
+
+            var ingridientsString = ingridients.Select(s => s._name).ToList();
+            options += string.Join(",", ingridientsString);
+            
+            return SearchRecipe(GetMinimalRecipies(options));
+        }
+        
+        public static Recipe SearchSingleRecipe(MinimalRecipe mR)
+        {
+            var n = new List<MinimalRecipe>();
+            n.Append(mR);
+            return SearchRecipe(n)[0];
+        }
+
+        public static List<Recipe> SearchRecipe(List<MinimalRecipe> minimalList)
+        {
+            var API_KEY = "7a98067ae9ea425ca548d96347913e74";
+            var url = "https://api.spoonacular.com/recipes/informationBulk?ids=";
+
+            var recipeString = minimalList.Select(s => s.id.ToString()).ToList();
+            url += string.Join(",", recipeString);
+            url += "&apiKey=" + API_KEY;
+
+            return JsonConvert.DeserializeObject<List<Recipe>>(get_request(url));
         }
 
         private static string get_request(string url)
@@ -49,48 +80,12 @@ namespace KitchenLib
                 return streamReader.ReadToEnd();
             }
         }
-
-        public List<Recipe> GetRecipies(uint number)
+        
+        private static List<MinimalRecipe> GetMinimalRecipies(string options)
         {
-            var call = "findByIngredients?ingredients=";
-
-            var ingridientsString = new List<string>();
-            foreach (var p in Ingridients)
-                ingridientsString.Append(p._name);
-
-            call += string.Join(",", ingridientsString);
-            call += "&number=";
-            call += number;
-
-            call += "&apiKey=";
-            call += API_KEY;
-            
-            var minimalList = JsonConvert.DeserializeObject<List<MinimalRecipe>>(get_request(apiRecepiesIngridients + call));
-
-            var recipeList = new List<Recipe>();
-            foreach (var mR in minimalList)
-            {
-                string reply = get_request(apiRecepiesIngridients + mR.id + "/information?includeNutrition=false");
-
-                var parsedObject = JObject.Parse(reply);
-                
-                var instructions = parsedObject["summary"].ToString();
-
-                var RP = new List<RecipeProduct>();
-                foreach (var PP in parsedObject["extendedIngredients"])
-                {
-                    RP.Append(new RecipeProduct(PP["id"].ToString(), PP["name"].ToString(), (uint) PP["measures"]["metric"]["amount"],
-                        PP["measures"]["metric"]["unitShort"].ToString()));
-                }
-
-
-                recipeList.Append(new Recipe(mR.title, mR.image, mR.id, RP, instructions));
-            }
-
-
-            return recipeList;
-
-
+            var API_KEY = "7a98067ae9ea425ca548d96347913e74";
+            string url = "https://api.spoonacular.com/recipes/complexSearch?" + options + "&apiKey=" + API_KEY;
+            return JsonConvert.DeserializeObject<RootMinimalRecipes>(get_request(url)).results;
         }
     }
 }
