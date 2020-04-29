3b61f3f8701bd46d2194c06f18322722202a3263
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-04-29 12:01:52 +0100

    improve recipe API wrapper

---
+++ KitchenApp/KitchenLib/MinimalRecipe.cs
@@ -0,0 +1,10 @@
+namespace KitchenLib
+{
+    public class MinimalRecipe
+    {
+        public string title { get; set; }
+        public string image { get; set; }
+        public uint id { get; set; }
+
+    }
+}
--- KitchenApp/KitchenLib/Recipe.cs
+++ KitchenApp/KitchenLib/Recipe.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Security.Cryptography;
 
 namespace KitchenLib
 {
@@ -8,38 +6,15 @@ namespace KitchenLib
     {
         public string id { get; set; }
         public string name { get; set; }
-        public uint ammount { get; set; }
-        public string unit { get; set; }
-
-        public RecipeProduct(string Id, string Name, uint Ammount, string Unit)
-        {
-            id = Id;
-            name = Name;
-            ammount = Ammount;
-            unit = Unit;
-        }
-
-
+        public string original{ get; set; }
     }
+    
     public class Recipe
     {
         public string title { get; set; }
         public string image { get; set; }
         public uint id { get; set; }
-        public List<RecipeProduct> Ingridients{ get; set; }
-        public string Preparation{ get; set; }
-
-        public Recipe(string recipeTitle, string imageURL, uint recipeId, List<RecipeProduct> ingridients, string preparation)
-        {
-            title = recipeTitle;
-            image = imageURL;
-            id = recipeId;
-            Ingridients = ingridients;
-            Preparation = preparation;
-        }
-
-        public Recipe()
-        {
-        }
+        public List<RecipeProduct> extendedIngredients{ get; set; }
+        public string summary{ get; set; }
     }
 }
