72753c544a0082d43a695995f734311629a54c46
Hitler <a85504@alunos.uminho.pt>
2020-05-09 11:49:27 +0100

    Add cors and docker support

--- KitchenApp/ShoppinglistService/Startup.cs
+++ KitchenApp/ShoppinglistService/Startup.cs
@@ -26,6 +26,17 @@ namespace ShoppinglistService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("http://localhost:1331", "http://localhost:3000")
+                        .AllowCredentials()
+                        .AllowAnyHeader()
+                        .WithExposedHeaders("auth")
+                        .AllowAnyMethod();
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +50,8 @@ namespace ShoppinglistService
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors();
 
             app.UseAuthorization();
 
