54440b25fe6f99c13e8a1f0067991b3e45fb0451
jtexeira <38398296+jtexeira@users.noreply.github.com>
2020-05-26 09:14:54 +0100

    Add DAO for Recipes, with capabilities to store favorite recipes (#87)

---
+++ KitchenApp/KitchenLib/Database/RecipeStore.cs
@@ -0,0 +1,134 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Neo4j.Driver;
+
+namespace KitchenLib.Database
+{
+    public class RecipeStore
+    {
+        public static async Task<bool> Exists(string uid)
+        {
+            bool exists;
+            var session = new Database("bolt://db:7687", "neo4j", "APPmvc").session();
+            try
+            {
+                exists = await session.ReadTransactionAsync(async tx =>
+                {
+                    var lst = new List<string>();
+                    var reader = await tx.RunAsync("MATCH(u:Recipe) WHERE u.id = $guid RETURN u.id",
+                        new {guid = uid});
+                    while (await reader.FetchAsync())
+                        lst.Add(reader.Current[0].ToString());
+
+                    return lst.Count != 0;
+                });
+            }
+            finally
+            {
+                await session.CloseAsync();
+            }
+
+            return exists;
+        }
+
+        public static async Task<MinimalRecipe> Get(string uid)
+        {
+            MinimalRecipe u = null;
+            var session = new Database("bolt://db:7687", "neo4j", "APPmvc").session();
+            try
+            {
+                await session.ReadTransactionAsync(async tx =>
+                {
+                    var reader = await tx.RunAsync("Match(u:Recipe) Where u.id = $guid Return u",
+                        new {guid = uid});
+                    u = new MinimalRecipe();
+                    while (await reader.FetchAsync())
+                    {
+                        var aa = reader.Current[0].As<INode>().Properties;
+                        foreach (var (key, value) in aa)
+                        {
+                            u.GetType().GetProperty(key)?.SetValue(u, value, null);
+                        }
+                    }
+                });
+            }
+            finally
+            {
+                await session.CloseAsync();
+            }
+
+            return u;
+        }
+        
+        public static async Task<MinimalRecipe> Add(MinimalRecipe r)
+        {
+            MinimalRecipe u = null;
+            var session = new Database("bolt://db:7687", "neo4j", "APPmvc").session();
+            try
+            {
+                await session.WriteTransactionAsync(async tx =>
+                {
+                    var reader = await tx.RunAsync("CREATE (i:Recipe {title: $title, id: $id, image: $image}) " +
+                                      "return i",
+                        new {r.id, r.image, r.title});
+                    u = new MinimalRecipe();
+                    while (await reader.FetchAsync())
+                    {
+                        var aa = reader.Current[0].As<INode>().Properties;
+                        foreach (var (key, value) in aa)
+                        {
+                            u.GetType().GetProperty(key)?.SetValue(u, value, null);
+                        }
+                    }
+                });
+            }
+            finally
+            {
+                await session.CloseAsync();
+            }
+
+            return u;
+        }
+
+        public static async Task star(string id, string user)
+        {
+            var session = new Database("bolt://db:7687", "neo4j", "APPmvc").session();
+            try
+            {
+                await session.WriteTransactionAsync(async tx =>
+                {
+                    var reader = await tx.RunAsync("Match(u:User) " +
+                                                   "Match(z:Recipe) " +
+                                                   "Where u._email = $email and z.id = $id " +
+                                                   "Optional match (u)-[f:REC]-(z) " +
+                                                   "with u, z, f, case when f is null then [1] else [] end as arr " +
+                                                   "foreach(x in arr | create (u)-[:REC]->(z))",
+                        new {email = user, id});
+                });
+            }
+            finally
+            {
+                await session.CloseAsync();
+            }
+        }
+
+        public static async Task unstar(string id, string user)
+        {
+            var session = new Database("bolt://db:7687", "neo4j", "APPmvc").session();
+            try
+            {
+                await session.WriteTransactionAsync(async tx =>
+                {
+                    var reader = await tx.RunAsync("Match(u:User)-[r:REC]->(z:Recipe) " +
+                                                   "where u._email = user and z.id = id " +
+                                                   "delete r",
+                        new {email = user, id});
+                });
+            }
+            finally
+            {
+                await session.CloseAsync();
+            }
+        }
+    }
+}
